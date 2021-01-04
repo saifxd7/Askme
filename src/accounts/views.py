e7d279016abfe2138a733e61d362065c6fc8a03f
kingsw0rd <you@example.com>
2021-01-04 20:01:46 +0530

    initial commit

---
+++ src/accounts/views.py
@@ -0,0 +1,112 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.contrib.auth import (
+    authenticate,
+    get_user_model,
+    login,
+    logout,
+
+)
+from django.shortcuts import render, redirect
+
+from django.contrib import messages
+from .forms import UserLoginForm, UserRegisterForm
+from django.contrib.auth.decorators import login_required
+import json
+import urllib
+import urllib.request
+from blog import settings
+
+
+def login_view(request):
+
+    # print(request.user.is_authenticated())
+    next = request.GET.get('next')
+    title = "Login"
+    form = UserLoginForm(request.POST or None)
+    if form.is_valid():
+
+        #   ''' Begin reCAPTCHA validation '''
+        recaptcha_response = request.POST.get('g-recaptcha-response')
+
+        url = 'https://www.google.com/recaptcha/api/siteverify'
+        values = {
+            'secret': settings.GOOGLE_RECAPTCHA_SECRET_KEY,
+            'response': recaptcha_response
+        }
+        data = urllib.parse.urlencode(values).encode()
+        req = urllib.request.Request(url, data=data)
+        response = urllib.request.urlopen(req)
+        result = json.loads(response.read().decode())
+        # ''' End reCAPTCHA validation '''
+
+        if result['success']:
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, 'Login Successfully.')
+            if next:
+                return redirect(next)
+            return redirect("/")
+
+        else:
+            messages.error(
+                request, 'Invalid reCAPTCHA. Please try again.')
+
+    return render(request, "form.html", {"form": form, "title": title})
+
+
+def register_view(request):
+    # print(request.user.is_authenticated())
+
+    next = request.GET.get('next')
+    title = "Register"
+    form = UserRegisterForm(request.POST or None)
+    if form.is_valid():
+
+        #   ''' Begin reCAPTCHA validation '''
+        recaptcha_response = request.POST.get('g-recaptcha-response')
+
+        url = 'https://www.google.com/recaptcha/api/siteverify'
+        values = {
+            'secret': settings.GOOGLE_RECAPTCHA_SECRET_KEY,
+            'response': recaptcha_response
+        }
+        data = urllib.parse.urlencode(values).encode()
+        req = urllib.request.Request(url, data=data)
+        response = urllib.request.urlopen(req)
+        result = json.loads(response.read().decode())
+        # ''' End reCAPTCHA validation '''
+
+        if result['success']:
+            user = form.save(commit=False)
+            password = form.cleaned_data.get('password')
+            user.set_password(password)
+            user.save()
+            new_user = authenticate(
+                username=user.username, password=password)
+            messages.success(request, 'Register Successfully.')
+            login(request, new_user)
+
+            if next:
+                return redirect(next)
+            return redirect("/")
+
+        else:
+            messages.error(
+                request, 'Invalid reCAPTCHA. Please try again.')
+
+    context = {
+        "form": form,
+        "title": title
+    }
+    return render(request, "form.html", context)
+
+
+@login_required
+def logout_view(request):
+    logout(request)
+    messages.success(request, 'Logout Successfully..')
+    return redirect("/")
---
+++ src/boards/views.py
@@ -0,0 +1,90 @@
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect, get_object_or_404
+from django.db.models import Count
+from django.contrib.auth.decorators import login_required
+# Create your views here.
+from .models import Board, Topic, Post
+from .forms import CreateTopicForm, PostForm
+
+
+@login_required
+def home(request):
+    boards = Board.objects.all()
+    return render(request, 'index.html', {'boards': boards})
+
+
+@login_required
+def board_topics(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    topics = board.topics.order_by(
+        '-last_updated').annotate(replies=Count('posts') - 1)
+    return render(request, 'topics.html', {'board': board, 'topics': topics})
+
+
+@login_required
+def create_topic(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    user = User.objects.first()  # get the currently logged in user
+    if request.method == 'POST':
+        form = CreateTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starter = user
+            topic.save()
+            post = Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                topic=topic,
+                created_by=user
+            )
+            # redirect to the created topic page
+            return redirect('board_topics', pk=board.pk)
+    else:
+        form = CreateTopicForm()
+    return render(request, 'create_topic.html', {'board': board, 'form': form})
+
+
+def topic_posts(request, pk, topic_pk):
+    topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
+    topic.views += 1
+    topic.save()
+    return render(request, 'topic_posts.html', {'topic': topic})
+
+
+@login_required
+def reply_topic(request, pk, topic_pk):
+    topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.topic = topic
+            post.created_by = request.user
+            post.save()
+            return redirect('topic_posts', pk=pk, topic_pk=topic_pk)
+    else:
+        form = PostForm()
+    return render(request, 'reply_topic.html', {'topic': topic, 'form': form})
+
+
+@login_required
+def create_question(request):
+    # board = get_object_or_404(Board, pk=pk)
+    user = User.objects.first()  # get the currently logged in user
+    if request.method == 'POST':
+        form = CreateTopicForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            # topic.board = board
+            topic.starter = user
+            question.save()
+            post = Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                question=question,
+                created_by=user
+            )
+            # redirect to the created topic page
+            return redirect('board_topics')
+    else:
+        form = CreateTopicForm()
+    return render(request, 'create_question.html', {'form': form})
