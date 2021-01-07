2dd2d76fb1c3ec1283d89e6dbcb308246740611a
kingsw0rd <you@example.com>
2021-01-07 14:55:26 +0530

    changed db

--- src/blog/settings.py
+++ src/blog/settings.py
@@ -90,11 +90,13 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'askme',
-        'USER': 'postgres',
-        'PASSWORD': 'password123',
-        'HOST': 'localhost',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': 'askme',
+        # 'USER': 'postgres',
+        # 'PASSWORD': 'password123',
+        # 'HOST': 'localhost',
     }
 }
 
